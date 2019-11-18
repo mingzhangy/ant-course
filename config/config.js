8df23c25c41a683e37cda40405326cec5975f068
zhangming <815597817@qq.com>
2019-11-18 19:39:22 +0800

    增加cards模块，桩数据等

--- config/config.js
+++ config/config.js
@@ -1,11 +1,33 @@
 export default {
+  theme: {
+    "@primary-color": '#40a9ff'
+  },
   plugins:[
     ['umi-plugin-react', {
-
+      antd: true,
+      dva: true,
+      locale: {
+        enable: true
+      }
     }]
   ],
   routes:[{
     path: '/',
-    component: './HelloWorld'
+    component: '../layout/BasicLayout',
+    routes: [{
+      path: '/helloworld',
+      component: './HelloWorld',
+    }, {
+      path: '/dashboard',
+      routes:[
+        {path: '/dashboard/analysis', component: './dashboard/Analysis'},
+        {path: '/dashboard/monitor', component: './dashboard/Monitor'},
+        {path: '/dashboard/workplace', component: './dashboard/Workplace'}
+      ]
+    }, {
+      path: '/puzzlecards', component: './puzzlecards' 
+    }, {
+      path: '/list', component: './list' 
+    }]
   }]
 }
